408649e48ec617e34a99d1d2d1e8e956357f5443
alexandrellemes <g1I@t3123@>
2020-03-21 15:28:54 -0300

    Configurações do ambiente

--- config.php
+++ config.php
@@ -29,9 +29,10 @@ $jsonDotEnv = json_encode($arrayDotEnv);
 $dotEnv = json_decode($jsonDotEnv);
 
 define('APP_ENV', $dotEnv->APP_ENV);
+define('ENVIRONMENT', $dotEnv->APP_ENV);
 define('APP_NAME', $dotEnv->APP_NAME);
 define('APP_KEY', $dotEnv->APP_KEY);
-define('APP_DEBUG', $dotEnv->APP_DEBUG);
+define('APP_DEBUG', $dotEnv->APP_DEBUG == 'true' ? true : false);
 define('APP_URL', $dotEnv->APP_URL);
 define('APP_VERSAO', $dotEnv->APP_VERSAO);
 
@@ -56,12 +57,44 @@ define('MAIL_REPLAYTO_NAME', $dotEnv->MAIL_REPLAYTO_NAME);
 
 define('SENDGRID_API_KEY', $dotEnv->SENDGRID_API_KEY);
 
+if (APP_DEBUG) {
+    ini_set('display_errors', true);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', false);
+    error_reporting(E_);
+}
 
-//Configuração do Banco de dados
-$host = "mysqldb";
-$user = "root";
-$pass = "123456";
-$d_b = "livro_caixa";
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
+switch (ENVIRONMENT) {
+    case 'development':
+        error_reporting(-1);
+        ini_set('display_errors', 1);
+        break;
+
+    case 'testing':
+    case 'production':
+        ini_set('display_errors', 0);
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+        break;
+
+    default:
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'The application environment is not set correctly.';
+        exit(1); // EXIT_ERROR
+}
+
 
 //Título do seu livro Caixa, geralmente seu nome
 $lc_titulo="Seu Nome";
@@ -74,5 +107,3 @@ $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("E
 $db = mysqli_select_db($conn, DB_DATABASE) or die("Erro na seleção da base de dados");
 
 //echo 'Conexão com sucesso.';
-
-?>
