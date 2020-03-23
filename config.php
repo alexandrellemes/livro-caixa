4bf24f6fca2eef8cf49d2790707465fd91acd262
alexandrellemes <g1I@t3123@>
2020-03-22 21:10:12 -0300

    Ajustes nas definições do .env.

--- config.php
+++ config.php
@@ -10,34 +10,7 @@ $arrayDotEnv = convertDotEnvArray($fileDotEnv);
 $jsonDotEnv = json_encode($arrayDotEnv);
 $dotEnv = json_decode($jsonDotEnv);
 
-define('APP_ENV', $dotEnv->APP_ENV);
 define('ENVIRONMENT', $dotEnv->APP_ENV);
-define('APP_NAME', $dotEnv->APP_NAME);
-define('APP_KEY', $dotEnv->APP_KEY);
-define('APP_DEBUG', $dotEnv->APP_DEBUG == 'true' ? true : false);
-define('APP_URL', $dotEnv->APP_URL);
-define('APP_VERSAO', $dotEnv->APP_VERSAO);
-
-define('DB_CONNECTION', $dotEnv->DB_CONNECTION);
-define('DB_HOST', $dotEnv->DB_HOST);
-define('DB_PORT', $dotEnv->DB_PORT);
-define('DB_DATABASE', $dotEnv->DB_DATABASE);
-define('DB_USERNAME', $dotEnv->DB_USERNAME);
-define('DB_PASSWORD', $dotEnv->DB_PASSWORD);
-
-define('MAIL_MAILER', $dotEnv->MAIL_MAILER);
-define('MAIL_HOST', $dotEnv->MAIL_HOST);
-define('MAIL_PORT', $dotEnv->MAIL_PORT);
-define('MAIL_USERNAME', $dotEnv->MAIL_USERNAME);
-define('MAIL_PASSWORD', $dotEnv->MAIL_PASSWORD);
-define('MAIL_ENCRYPTION', $dotEnv->MAIL_ENCRYPTION);
-define('MAIL_FROM_ADDRESS', $dotEnv->MAIL_FROM_ADDRESS);
-define('MAIL_FROM_NAME', $dotEnv->MAIL_FROM_NAME);
-
-define('MAIL_REPLAYTO_MAIL', $dotEnv->MAIL_REPLAYTO_MAIL);
-define('MAIL_REPLAYTO_NAME', $dotEnv->MAIL_REPLAYTO_NAME);
-
-define('SENDGRID_API_KEY', $dotEnv->SENDGRID_API_KEY);
 
 if (APP_DEBUG) {
     ini_set('display_errors', true);
@@ -77,9 +50,6 @@ switch (ENVIRONMENT) {
         exit(1); // EXIT_ERROR
 }
 
-
-//Título do seu livro Caixa, geralmente seu nome
-$lc_titulo="Seu Nome";
 
 //////////////////////////////////////
 //Não altere a partir daqui!
